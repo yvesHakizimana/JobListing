5728c7bbcdc22b2c7d3d70c48a5b18fd9f30c4e1
Yves HAKIZIMANA <yvhakizimana123@gmail.com>
2024-04-21 10:19:37 +0200

    Initializing the Application

---
+++ src/main/java/com/code/joblisting/JobListingPlatformApplication.java
@@ -0,0 +1,15 @@
+package com.code.joblisting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class JobListingPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobListingPlatformApplication.class, args);
+    }
+
+
+}
---
+++ src/test/java/com/code/joblisting/JobListingPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.code.joblisting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobListingPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
